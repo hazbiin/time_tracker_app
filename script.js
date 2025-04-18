09afcca1ffa8b8ffd8b026279e8a673bb73b802f
hasbin <hzbi717@gmail.com>
2025-04-18 15:07:28 +0530

    working on task and timer management

--- script.js
+++ script.js
@@ -1,87 +1,184 @@
-const createTaskButton = document.getElementById('create-task-button');
+
+// localStorage.clear();
+// creating new task pop up..
+const createTaskPopupButton = document.getElementById('create-task-popup-btn');
 const createNewTaskPopup = document.getElementById('create-new-task-popup');
-// const tasksSectionDescription = document.getElementById("tasks-section-description");
-const taskSection = document.getElementById("tasks-section");
+const createTaskButton = document.getElementById("create-task-button");
+
+// intro description..
+const tasksSectionDescription = document.getElementById("tasks-section-desc");
+
+// getting saved tasks from localStorage..
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+console.log(tasks);
 
 
-// task creation inputs ..
-const taskNameInput = document.getElementById("task-name");
-const taskDescInput = document.getElementById("task-desc");
-const saveTaskButton = document.getElementById("save-task-button");
+// ///////////implementing timer////////////////////////
+// variables for time values 
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// variables for leading zeros
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+// variables for set inteval and timer status
+let timerInterval = null;
+let timerStatus = "stopped";
 
 
-let task;
-createTaskButton.addEventListener("click", () => {
-    createNewTaskPopup.classList.toggle('show');
-    // taskSection.classList.toggle('hide');
+// listing tasks if any from local storage////
+if(tasks.length !== 0) {
+    tasksSectionDescription.classList.add('hide');
+    
+    let newTasksList = document.getElementById("new-tasks-list");
+    newTasksList.innerHTML = "";
+
+    tasks.forEach(task => {
+        newTasksList.innerHTML += `
+            <li class="task-list-item">
+                <div class="task-info">
+                    <div class="list-item-text-section">
+                        <h4>${task.taskName}</h4>
+                        <p>0hrs 0mins 0secs</p>
+                        <p>${task.taskStatus}</p>
+                    </div>
+                    <div class="list-item-buttons-section">
+                        <button id="timer-btn">start timer</button>
+                        <button id="edit-btn">edit</button>
+                    </div>
+                </div>
+
+                <div id="timer-container" class="timer-container">
+                    <h3 id="timer"></h3> 
+                </div>
+            </li>
+    `;
+    
+    // console.log(task)  
+    })
+
+    const timerBtns = document.querySelectorAll("#timer-btn");
+    timerBtns.forEach(timerBtn =>{
+        timerBtn.addEventListener("click", () => {
+
+            // let displayTimer = document.getElementById("timer");
+            const timerContainer = document.getElementById("timer-container");
+            console.log(timerContainer)
+            timerContainer.classList.add("started");
+
+            const now = new Date();
+            // let time = {}; 
+
+            if(timerStatus === "stopped") {   
+                timerInterval = window.setInterval(timer, 1000);
+                document.getElementById("timer-btn").innerHTML= "stop timer";
+                timerStatus = "started";
+                
+                // time["startTime"] = now.toLocaleTimeString();
+                // console.log(time)
+            }else {
+                window.clearInterval(timerInterval);
+                document.getElementById("timer-btn").innerHTML= "start timer";
+                timerStatus = "stopped";
+    
+                // time["endTime"] = now.toLocaleTimeString();
+                // console.log(time)
+            }
+
+            // console.log(time);
+        })
+    })
+}
 
 
 
-    // listing tasks...
-    // the task and all other data must be listed by taking it from local storage.
-    if(task) {
-        // const newTasksContainer = document.getElementById("new-tasks-container");
+// timer function
+function timer() {
+    seconds ++;
 
-        const newTaskslist = document.getElementById("new-tasks-list");
-        // console.log(newTaskslist)
-
-        // newTaskslist.innerHTML += `<li>
-        //                     <h6>${task.name}</h6>
-        //                     </li>`
-        // task = null;
-
-        newTaskslist.innerHTML += `
-                     <li class="task-list-item">
-
-                        <div class="list-item-top">
-                            <div class="list-text-section">
-                                <h4>working on project</h4>
-                                <p>0hrs 0mins 0secs</p>
-                                <p>in-progress...</p>
-                            </div>
-                            <div class="list-item-buttons">
-                                <button id="timer-btn">start timer</button>
-                                <button id="edit-btn">edit</button>
-                            </div>
-                        </div>
-
-                        <div class="list-item-bottom">
-                            <div><a href="#">view more.. </a></div>
-                        </div>
-                    
-                    </li>
-        `;
-
-        task = null;
+    if(seconds === 60) {
+        seconds = 0;
+        minutes++;
+        if(minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
     }
+
+    if(seconds < 10) {
+        leadingSeconds = "0" + seconds.toString();
+    }else {
+        leadingSeconds = seconds;
+    }
+
+    if(minutes < 10) {
+        leadingMinutes = "0" + minutes.toString();
+    }else {
+        leadingMinutes = minutes
+    }
+
+    if(hours < 10) {
+        leadingHours = "0" + hours.toString();
+    }else {
+        leadingHours = hours;
+    }
+
+
+    let displayTimer = document.getElementById("timer");
+    displayTimer.innerText = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`;
+}
+
+
+
+
+
+// creating new tasks //////////////
+createTaskPopupButton.addEventListener("click", () => {
+    createNewTaskPopup.classList.add('show');
+
+    const closeCreateTaskPopup = document.getElementById('close-create-task-pop-up-btn');
+    closeCreateTaskPopup.addEventListener("click", () => {
+        createNewTaskPopup.classList.remove('show');
+    })
 })
 
+if(createTaskButton){
 
-// edit button implementation.
-// show the pop up 
-// allow user to edit whatever he needs
-// on clciking save button, we wanna check if there is a change for each task object property, 
-// if there is change means update that, else keep no change. 
+    // task creation inputs ..
+    const taskNameInput = document.getElementById("task-name");
+    const taskDescInput = document.getElementById("task-desc");
+    const taskTagInput = document.getElementById("task-tag");
+    const taskStatusInput = document.getElementById("task-status"); 
+
+    createTaskButton.addEventListener("click", () => {
+        const taskName = taskNameInput.value;
+        const taskDesc = taskDescInput.value;
+        const taskTag = taskTagInput.value;
+        const taskStatus = taskStatusInput.value;
+    
+        taskNameInput.value = "";
+        taskDescInput.value = "";
+        taskTagInput.value = "";
+        taskStatusInput.value = "";
+
+        
+        const newTask = {
+            "taskName" : taskName,
+            "taskDesc" : taskDesc,
+            "taskTag" : taskTag,
+            "taskStatus" : taskStatus
+        }
+        
+        tasks.push(newTask);
+        
+        // storing to local storage.
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    })
+}
 
 
-
-// task creation , 
-// after the task creation we wanna save that to the local storage.
-
-saveTaskButton.addEventListener("click", () => {
-    const taskName = taskNameInput.value;
-    const taskDesc = taskDescInput.value;
-  
-    taskNameInput.value = "";
-    taskDescInput.value = "";
-
-    task = {
-        name : taskName,
-        desc : taskDesc
-    }
-
-
-    // you have to save this object to local storage.
-    console.log(task)
-})
-
+// clearing tasks on development mode.
+// localStorage.clear();
---
+++ timeapp.js
@@ -0,0 +1,148 @@
+// i need a time managment app.
+
+// login
+// first view calender of current month.
+// choose date 
+
+// iam planning a calender view
+// first it shoulb be like blocks of calebder, it must wide into a whole a year callender.
+
+// focused on current date, and can view future dates to scedule tasks, 
+// old dates can be accessible, but cant do any edits, but we can see the insights of that day. 
+
+// old day history,---?????
+// list out tasks, 
+// -- number based on task with highest time. , show percentages of each in a bar 
+
+
+
+// add new task,   ------- create, edit, delete, 
+// enter start time 
+// end time 
+// additional notes about the task
+// total time
+
+// allow user to categorize the task based on priority. 
+// if there got any pending task in a day. add that task to the next day list and ask user if he wanna do that that day or any other day or discard it.
+
+
+
+// future scheduled tasks, 
+// give an option to add new tasks, 
+// when that day occur, get notified about that task, 
+// and put that task as higher priority in that day.
+
+
+
+// on each new day user should get the insight about how he spend all his previous day.. 
+// -- task spent with more time 
+// -- least time 
+// -- compare progress with previous day 
+// -- just keep a standard for saying how productive his day was.
+
+
+// he should be able to compare his own productivity on different days
+
+
+
+// keep track of the tasks user is doing,
+// ---- notice most done task
+// ---- notice least done task 
+// if the user doesnt do the most frequent task, he must be notifed to do, at the end of the day.
+
+
+// collab and manage time. 
+// group study 
+// team work, 
+
+
+
+// time tracker + task tracker
+// learning- react - tag   in-progress --  ///timer start - timer stop///
+// calendar view to view tasks and time of each date.
+
+// task object 
+let task = {
+    name: "javascript",
+    description: "jldklfksl",
+    taskTag : studies,
+    time: [
+        {
+            slot: 1,
+            startTime: 5785,
+            endTime: 590,
+            totalTime: 78787
+        },
+        {
+            slot: 2,
+            startTime: 5785,
+            endTime: 590,
+            totalTime: 78787
+        },
+        {
+            slot: 3,
+            startTime: 5785,
+            endTime: 590,
+            totalTime: 78787
+        },
+    ],
+    totalTime : 8989898,
+    taskStatus : 'inprogress/ done'
+}
+
+// timer should be a seperate function.
+// arguments, name of the task- to get it displayed while timer is ticking. 
+
+// create tasks --- 
+// name and description fields
+// option to edit the name of the task.
+// option to tag the task.
+
+// completion note --- in progress or done --- allow user to mannually track it down.
+
+// start doing --- timer should start --
+
+// show timer in big view - with the the name of it.
+
+// stop timer --
+// then the start time and end time with its total time should list under the tasks.
+// find some creative way to display it.
+
+// add an option to mannuallly enter the start time and stop, if incase user missed to use the timer.
+// add an option to make note of additional dialy stuffs, the user do.
+
+
+// daily summary---
+// at each morning -- just show a graph of each task with percentage of time spent for each task.
+// if iam making a daily summary means, i can make use of it to mannually search each dates and see the status of that day.
+
+
+// history---
+// daily status history should persist.
+// weekly insights, 
+// monthly insights,
+
+
+// seprate view of all the tasks.
+// show all the task objects.
+// default grouping is done by date.
+// search bar, search by tags.
+// show the total time of tasks in that particular searched tag.
+
+// oru task eduthal 
+// ath day vech time slots vech sum chyth kaanikenm.
+
+
+// what are you gonna create?
+// first i will allow user to create task...
+
+// date view 
+// task ----------- title,description,timer, in-progress/done., tag.
+
+
+
+// edit button implementation.
+// show the pop up 
+// allow user to edit whatever he needs
+// on clciking save button, we wanna check if there is a change for each task object property, 
+// if there is change means update that, else keep no change. 
